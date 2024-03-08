edca7d9a3342ce38d5ef2cd9f1c98dfd0656230f
Thevambikai <110016117+Thevambikai@users.noreply.github.com>
2024-03-08 18:18:26 +0530

    Modify file to listen msg and update dashboard

--- dashboard/src/contexts/machinecontext.js
+++ dashboard/src/contexts/machinecontext.js
@@ -2,10 +2,11 @@
 import { createContext, useContext, useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import socketIOClient from 'socket.io-client';
 
 const MachineContext = createContext();
 
-// const { io } = require("socket.io-client");
+const { io } = require("socket.io-client");
 
 export const MachineProvider = ({ children }) => {
   const [machineData, setMachineData] = useState();
@@ -22,27 +23,76 @@ export const MachineProvider = ({ children }) => {
     }
   };
 
-  // const setupSocketListener = () => {
-  //   const socket = io(process.env.REACT_APP_SERVER_BASE_URL, {
-  //     transports: ["websocket"],
-  //   });
-  //   socket.on("machineDataUpdate", (data) => {
-  //     // Update the machineData state with real-time data from the socket
-  //     setMachineData(data);
-  //     console.log("Socket update:", data);
-  //   });
 
-  //   socket.on("machines", (data) => {
-  //     console.log("Connected to socket");
-  //     console.log("socket data ", data);
-  //   });
 
-  //   socket.on("disconnect", () => {
-  //     console.log("Disconnected from socket");
-  //   });
-  // };
+//   useEffect(() => {
+    
+
+//     return () => {
+//         socket.disconnect();
+//     };
+// }, []);
+
+
+  const setupSocketListener = () => {
+    const socket = io("http://localhost:3001");
+    
+    socket.on('updated_protector_messages', (updatedMessages) => {
+      if (updatedMessages.type == "run" )
+      setMachineData(state => {
+        return state.map(machine => {
+          if (machine.moldProtector == updatedMessages.protector_id){
+            console.log("thanujan", updatedMessages.data.success)
+            if (updatedMessages.data.success == 1)
+              return {...machine, moldShots : machine.moldShots + 1  }
+            else (updatedMessages.data.success == 0)
+              return {...machine, failedShots : machine.failedShots + 1  }
+          }
+          else 
+            return machine
+        })
+      })
+
+      else if (updatedMessages.type == "status" )
+      setMachineData(state => {
+        return state.map(machine => {
+          if (machine.moldProtector == updatedMessages.protector_id){
+            if(updatedMessages.data.status == "working" || updatedMessages.data.status == "stuck" || updatedMessages.data.status == "error" )
+              return {...machine,  status : updatedMessages.data.status}
+            else 
+              return machine 
+          }
+          else 
+            return machine
+        })
+      })
+
+      
+
+
+      console.log(updatedMessages, "thanujan")
+
+     
+
+        // setProtectorMessages(updatedMessages);
+        // Object.entries(updatedMessages).forEach(([protectorId, messages]) => {
+        //     messages.forEach(message => {
+        //         console.log(`Message received from Protector ${protectorId}: total_shots: ${message.total_shots}, failed_shots: ${message.failed_shots}`);
+        //     });
+
+            
+        // });
+    });
+    
+
+    
+  };
+
+
   useEffect(() => {
     fetchDataFromBackend(); // Fetch initial data when the component mounts
+    setupSocketListener();
+  
   }, []);
 
   return (
